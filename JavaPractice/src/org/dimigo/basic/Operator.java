bd5cecc26c0809bc47aa52a5b6452bd5f9ab06eb
ldcle <ldcle@DESKTOP-EAUBS8C>
2017-03-13 10:29:52 +0900

    실습과제2 제출

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_Operator
+ *
+ * 1. 개요 : 디미베네
+ * 2. 작성일 : 2017. 3. 13.
+ * </pre>
+ * 
+ * @author : 이동찬
+ * @version : 1.0
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a = 1700000;
+		int b = 3, c = 1500;
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : " + String.format("%,d", a) + "원");
+		System.out.printf("점포 내 직원 수 : %d명\n", b);
+		System.out.println("점포 수 : " + String.format("%,d", 1500) + "원");
+		System.out.println("");
+		System.out.println("연간 인건비 : " + String.format("%,d", (long)a*12*b*c) + "원");
+
+	}
+
+}
