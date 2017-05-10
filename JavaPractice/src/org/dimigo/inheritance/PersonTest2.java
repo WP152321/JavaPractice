20c264a33acdd70584a226bca11d3c00ca7416f5
ldcle <ldcle@DESKTOP-EAUBS8C>
2017-05-10 12:46:06 +0900

    실습과제10 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_PersonTest2
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 10.
+ * </pre>
+ * 
+ * @author : ldcle
+ * @version : 1.0
+ */
+public class PersonTest2 {
+
+	public static void main(String[] args) {
+		Person[] persons = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍"),
+		};
+		
+		for(Person p : persons){
+			greeting(p);
+		}
+		
+	}
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+
+}
