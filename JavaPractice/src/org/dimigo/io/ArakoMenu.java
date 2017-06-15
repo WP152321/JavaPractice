def1672a820e5af16ed4a8e98a3c92ccb1585753
ldcle <ldcle@DESKTOP-EAUBS8C>
2017-06-15 11:41:36 +0900

    실습과제17 제출

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.dimigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.Writer;
+
+/**
+ * <pre>
+ * org.dimigo.io
+ *   |_ArakoMenu
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 15.
+ * </pre>
+ * 
+ * @author : ldcle
+ * @version : 1.0
+ */
+public class ArakoMenu {
+
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+			BufferedWriter writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+			String data;
+			
+			while((data = br.readLine()) != null){
+				writer.write(data);
+				writer.newLine();
+			}
+			
+			writer.flush();
+				
+			System.out.println("<<메뉴 출력>>");
+			
+			while((data = reader.readLine()) != null){
+				System.out.println(data);
+			}
+			
+		} catch (Exception e) { 
+		 e.printStackTrace(); }
+	}
+}
